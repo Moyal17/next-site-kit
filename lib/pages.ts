0948c797cc6ade3ca5ca358e8b114d298f67468e
Dor Moyal <moyal17@gmail.com>
2023-08-17 11:55:37 +0300

    ignoreDuringBuilds

--- lib/pages.ts
+++ lib/pages.ts
@@ -1,19 +1,15 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-
 export async function getPageData(id: string) {
     const fullPath = path.join('staticData/pages', `${id}.md`);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
-
     // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents);
-
     const blogPostWithHTML: Response = {
         id,
         title: matterResult.data.title,
         data: matterResult.data,
     }
-
     return blogPostWithHTML
 }
